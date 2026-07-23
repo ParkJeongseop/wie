2c3cf7fc1134e3998647ff4271c3292e597092b1
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2026-07-23 20:23:08 +0900

    feat(skvm): add Device.isKeyToneEnabled

--- wie_skvm/src/classes/com/skt/m/device.rs
+++ wie_skvm/src/classes/com/skt/m/device.rs
@@ -19,6 +19,7 @@ impl Device {
                 JavaMethodProto::new("setColorMode", "(I)V", Self::set_color_mode, MethodAccessFlags::STATIC),
                 JavaMethodProto::new("isBacklightEnabled", "()Z", Self::is_backlight_enabled, MethodAccessFlags::STATIC),
                 JavaMethodProto::new("setBacklightEnabled", "(Z)V", Self::set_backlight_enabled, MethodAccessFlags::STATIC),
+                JavaMethodProto::new("isKeyToneEnabled", "()Z", Self::is_key_tone_enabled, MethodAccessFlags::STATIC),
                 JavaMethodProto::new("setKeyToneEnabled", "(Z)V", Self::set_key_tone_enabled, MethodAccessFlags::STATIC),
                 JavaMethodProto::new("enableRestoreLCD", "(Z)V", Self::enable_restore_lcd, MethodAccessFlags::STATIC),
                 JavaMethodProto::new("setKeyRepeatTime", "(II)V", Self::set_key_repeat_time, MethodAccessFlags::STATIC),
@@ -44,6 +45,12 @@ impl Device {
         tracing::warn!("stub com.skt.m.Device::setBacklightEnabled({enabled:?})");
 
         Ok(())
+    }
+
+    async fn is_key_tone_enabled(_jvm: &Jvm, _context: &mut WieJvmContext) -> JvmResult<bool> {
+        tracing::warn!("stub com.skt.m.Device::isKeyToneEnabled()");
+
+        Ok(true)
     }
 
     async fn set_key_tone_enabled(_jvm: &Jvm, _context: &mut WieJvmContext, enabled: bool) -> JvmResult<()> {
